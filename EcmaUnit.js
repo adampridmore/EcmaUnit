6205c77c7041a9d8f109030cf4876b43a288fbb9
Adam Pridmore <adam_pridmore@hotmail.com>
2014-10-02 12:06:30 +0100

    - Added code to run and report passed and failed tests.

--- EcmaUnit.js
+++ EcmaUnit.js
@@ -1,9 +1,55 @@
 function EcmaUnitRunner(){
-	this.run = function(testFixture){
-		for(property in testFixture){
-			if (typeof(testFixture[property] === "function")){
-				testFixture[property]();
-			}
-		}
-	}
+  this.run = function(testFixture){
+    var isFunction = function(fn){
+      return (typeof fn === "function");
+    };
+
+    var runTest = function(test, fixtureResult){
+      try{
+        test();
+        fixtureResult.testResults.push({
+          result: "pass", 
+          test: test
+        });
+        fixtureResult.passCount++;
+
+      }catch(exception){
+        fixtureResult.testResults.push({
+          result: "fail", 
+          test: test,
+          exception: exception
+        });
+        fixtureResult.failCount++;
+      }
+    };
+
+    var setPassedFlag = function(fixtureResult){
+      if (fixtureResult.failCount === 0){
+        fixtureResult.passed = true;
+      } else {
+        fixtureResult.passed = false;
+      }
+    }
+
+    var runFixtureInternal = function(testFixture){
+      var fixtureResult = {
+        testResults: [],
+        passCount: 0,
+        failCount: 0
+      };
+
+      for(property in testFixture){
+        var test = testFixture[property];
+        if (isFunction(test)){
+          runTest(test, fixtureResult);
+        }
+      }
+
+      setPassedFlag(fixtureResult);
+
+      return fixtureResult;
+    };
+
+    return runFixtureInternal(testFixture);
+  }
 };
--- EcmaUnitTests_MongoDB.js
+++ EcmaUnitTests_MongoDB.js
@@ -10,16 +10,36 @@ function test1(){
     }
   };
 
-  runner.run(testFixture);
+  var result = runner.run(testFixture);
 
   assert(test1WasCalled, 'Expected test1WasCalled');
+  assert(result.passed === true, 'Expected result.passed was true');
+  assert(result.passCount === 1, 'Expected result.passCount was 1');
+  assert(result.failCount === 0, 'Expected result.failCount was 0');
+}
+
+function test2_failingTest(){
+  var runner = new EcmaUnitRunner();
+
+  var testFixture = {
+    test1: function(){
+      throw "test2_error";
+    }
+  };
+
+  var result = runner.run(testFixture);
+
+  assert(result, "Runner did not return result");
+  assert(result.passed === false, "Runner did not return result.passed of false");
+  assert(result.passCount === 0, 'Expected result.passCount was 0');
+  assert(result.failCount === 1, 'Expected result.failCount was 1');
 }
 
 main();
 
 function main(){
-  var tests = [test1];
-  
+  var tests = [test1, test2_failingTest];
+
   var failed = false;
 
   tests.forEach(function(test){

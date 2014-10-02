3c3e50bee4edc1b69df6f380f80120d867c209ff
Adam Pridmore <adam_pridmore@hotmail.com>
2014-10-02 11:37:16 +0100

    - Refactored the unit tests to make it easier to add more tests.

--- EcmaUnitTests_MongoDB.js
+++ EcmaUnitTests_MongoDB.js
@@ -1,15 +1,50 @@
 load('EcmaUnit.js');
 
-var runner = new EcmaUnitRunner();
+function test1(){
+  var runner = new EcmaUnitRunner();
 
-var test1WasCalled = false;
-var testFixture = {
-	test1: function(){
-		test1WasCalled = true;
-	}
-};
+  var test1WasCalled = false;
+  var testFixture = {
+    test1: function(){
+      test1WasCalled = true;
+    }
+  };
 
-runner.run(testFixture);
+  runner.run(testFixture);
 
-assert(test1WasCalled, "Expected test1WasCalled");
-print("Passed!");
+  assert(test1WasCalled, 'Expected test1WasCalled');
+}
+
+main();
+
+function main(){
+  var tests = [test1];
+  
+  var failed = false;
+
+  tests.forEach(function(test){
+    try{
+      test();
+    }catch(e){
+      failed = true;
+      print('Test failed: ' + functionName(test));
+      print(e);
+    }   
+  });
+
+  print();
+  if (failed){
+    print('************');
+    print('Tests Failed');
+    print('************');
+  }else {
+    print('Passed');
+  }
+}
+
+function functionName(fun) {
+  var ret = fun.toString();
+  ret = ret.substr('function '.length);
+  ret = ret.substr(0, ret.indexOf('('));
+  return ret;
+}
--- runTests.bat
+++ runTests.bat
@@ -1,1 +1,1 @@
-mongo --nodb EcmaUnitTests_MongoDB.js
+mongo --quiet --nodb EcmaUnitTests_MongoDB.js

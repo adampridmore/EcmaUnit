b7c1cfd9a1ee0452b2bedacf578a825ab332bc63
Adam Pridmore <adam_pridmore@hotmail.com>
2014-10-06 21:52:31 +0100

    - Unit tests run all methods in fixture class.

--- EcmaUnitTests_MongoDB.js
+++ EcmaUnitTests_MongoDB.js
@@ -1,121 +1,115 @@
 load('EcmaUnit.js');
 
-function test1_passingTest(){
-  var runner = new ecmaUnit.Runner();
+var tests = {
+  test1_passingTest : function(){
+    var runner = new ecmaUnit.Runner();
 
-  var test1WasCalled = false;
-  var testFixture = {
-    test1: function(){
-      test1WasCalled = true;
+    var test1WasCalled = false;
+    var testFixture = {
+      test1: function(){
+        test1WasCalled = true;
+      }
+    };
+
+    var result = runner.run(testFixture);
+
+    assert(test1WasCalled, 'Expected test1WasCalled');
+    assert(result.passed === true, 'Expected result.passed was true');
+    assert(result.passCount === 1, 'Expected result.passCount was 1');
+    assert(result.failCount === 0, 'Expected result.failCount was 0');
+    assert(result.testResults.length === 1);
+    assert(result.testResults[0].result === 'pass');
+    //assert(result.testResults[0].testName === 'test1', result.testResults[0].testName);
+  },
+
+  test2_failingTest : function(){
+    var runner = new ecmaUnit.Runner();
+
+    var testFixture = {
+      test1: function(){
+        throw 'test2_error';
+      }
+    };
+
+    var result = runner.run(testFixture);
+
+    assert(result, "Runner did not return result");
+    assert(result.passed === false, "Runner did not return result.passed of false");
+    assert(result.passCount === 0, 'Expected result.passCount was 0');
+    assert(result.failCount === 1, 'Expected result.failCount was 1');
+    assert(result.testResults.length === 1);
+    assert(result.testResults[0].result === "fail");
+  },
+
+  test3_printTestResults : function(){
+    var runner = new ecmaUnit.Runner();
+
+    var testFixture = {
+      test1: function(){
+        throw 'test2_error';
+      },
+      test2: function(){
+        // Passes
+      }
+    };
+
+    var result = runner.run(testFixture);
+    //printjson(result);    
+
+    var actualText = result.stringify();
+    //printjson(result);
+    var expectedText = 'Test Results\r\n============\r\nRan\r\ntest1 - fail\r\ntest2 - pass';
+
+    assert(actualText === expectedText, 'Incorrect actualText:\r\n' + actualText + '\r\nExpected\r\n' + expectedText);
+  },
+
+  test4_assertEquals_when_equal : function(){
+    assert.areEqual('a', 'a');
+    assert.areEqual(1, 1);
+    assert.areEqual(1.1, 1.1);
+    assert.areEqual(true, true);
+    assert.areEqual(null, null);
+    assert.areEqual(undefined, undefined);
+    
+    var a = {};
+    var b = a;
+    assert.areEqual(a, b);
+  },
+
+  test4_assertEquals_when_not_equal : function(){
+    try{
+      assert.areEqual('a', 'b');
+      assert(false, 'Expected exception to be thrown');
+    }catch(e){
+      assert(e === "Expected 'a' but was 'b'", e);
+    }  
+  },
+
+  test4_assertEquals_when_not_equal_with_message : function(){
+    try{
+      assert.areEqual('a', 'b', "MyErrorMessage");
+      assert(false, 'Expected exception to be thrown.');
+    }catch(e){
+      assert(e === "Expected 'a' but was 'b'\r\nMessage: MyErrorMessage", e);
     }
-  };
-
-  var result = runner.run(testFixture);
-
-  assert(test1WasCalled, 'Expected test1WasCalled');
-  assert(result.passed === true, 'Expected result.passed was true');
-  assert(result.passCount === 1, 'Expected result.passCount was 1');
-  assert(result.failCount === 0, 'Expected result.failCount was 0');
-  assert(result.testResults.length === 1);
-  assert(result.testResults[0].result === 'pass');
-  //assert(result.testResults[0].testName === 'test1', result.testResults[0].testName);
-}
-
-function test2_failingTest(){
-  var runner = new ecmaUnit.Runner();
-
-  var testFixture = {
-    test1: function(){
-      throw 'test2_error';
-    }
-  };
-
-  var result = runner.run(testFixture);
-
-  assert(result, "Runner did not return result");
-  assert(result.passed === false, "Runner did not return result.passed of false");
-  assert(result.passCount === 0, 'Expected result.passCount was 0');
-  assert(result.failCount === 1, 'Expected result.failCount was 1');
-  assert(result.testResults.length === 1);
-  assert(result.testResults[0].result === "fail");
-}
-
-function test3_printTestResults(){
-  var runner = new ecmaUnit.Runner();
-
-  var testFixture = {
-    test1: function(){
-      throw 'test2_error';
-    },
-    test2: function(){
-      // Passes
-    }
-  };
-
-  var result = runner.run(testFixture);
-  //printjson(result);    
-
-  var actualText = result.stringify();
-  //printjson(result);
-  var expectedText = 'Test Results\r\n============\r\nRan\r\ntest1 - fail\r\ntest2 - pass';
-
-  assert(actualText === expectedText, 'Incorrect actualText:\r\n' + actualText + '\r\nExpected\r\n' + expectedText);
-}
-
-function test4_assertEquals_when_equal(){
-  assert.areEqual('a', 'a');
-  assert.areEqual(1, 1);
-  assert.areEqual(1.1, 1.1);
-  assert.areEqual(true, true);
-  assert.areEqual(null, null);
-  assert.areEqual(undefined, undefined);
-  
-  var a = {};
-  var b = a;
-  assert.areEqual(a, b);
-}
-
-function test4_assertEquals_when_not_equal(){
-  try{
-    assert.areEqual('a', 'b');
-    assert(false, 'Expected exception to be thrown');
-  }catch(e){
-    assert(e === "Expected 'a' but was 'b'", e);
-  }  
-}
-
-function test4_assertEquals_when_not_equal_with_message(){
-  try{
-    assert.areEqual('a', 'b', "MyErrorMessage");
-    assert(false, 'Expected exception to be thrown.');
-  }catch(e){
-    assert(e === "Expected 'a' but was 'b'\r\nMessage: MyErrorMessage", e);
-  }
+  },
 }
 
 main();
 
 function main(){
-  var tests = [
-    test1_passingTest, 
-    test2_failingTest,
-    test3_printTestResults,
-    test4_assertEquals_when_equal,
-    test4_assertEquals_when_not_equal,
-    test4_assertEquals_when_not_equal_with_message
-  ];
-
+  
   var failed = false;
 
-  tests.forEach(function(test){
+  for(var testName in tests){
     try{
-      test();
+      tests[testName]();
     }catch(e){
       failed = true;
-      print('Test failed: ' + functionName(test));
+      print('Test failed: ' + testName);
       print(e);
-    }   
-  });
+    }
+  };
 
   print();
   if (failed){
